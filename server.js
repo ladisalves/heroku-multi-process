3bd5198097e1bf521d57a0ce5df5727b1a8869de
Ladislav Stefan <ladislaves@gmail.com>
2021-11-18 10:52:33 +0100

    multi process in one file

--- server.js
+++ server.js
@@ -2,7 +2,12 @@
 const express = require('express');
 const app = express();
 const port = 4000;
+const port2 = 5000;
 
 app.listen(port, () => {
-  console.log(`Success! Your application is running on port ${port}.`);
+	console.log(`Success! Your application is running on port ${port}.`);
+});
+
+app.listen(port2, () => {
+	console.log(`Success! Your application is running on port ${port2}.`);
 });
